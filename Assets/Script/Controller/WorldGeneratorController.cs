07c025324e6fd283f0db0caf7757941df30b0c4c
Dauler Palhares <daulerpalhares@gmail.com>
2017-08-23 21:06:18 -0300

    Todo

---
+++ Assets/Script/Controller/WorldGeneratorController.cs
@@ -0,0 +1,11 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WorldGeneratorController.cs" company="Dauler Palhares">
+//  © Copyright Dauler Palhares da Costa Viana 2017.
+//          http://github.com/AguaMolhada
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+public class WorldGeneratorController {
+
+
+
+}
